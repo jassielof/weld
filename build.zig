baea49bd19287fd60ff359df4ad3426bfb3ba950
Jassiel Ovando <jassielovando@protonmail.com>
2026-02-20 20:13:51 -0400

    worskpace

--- build.zig
+++ build.zig
@@ -1,20 +1,10 @@
 const std = @import("std");
 
-// Although this function looks imperative, it does not perform the build
-// directly and instead it mutates the build graph (`b`) that will be then
-// executed by an external runner. The functions in `std.Build` implement a DSL
-// for defining build steps and express dependencies between them, allowing the
-// build runner to parallelize the build automatically (and the cache system to
-// know when a step doesn't need to be re-run).
+/// Although this function looks imperative, it does not perform the build directly and instead it mutates the build graph (`b`) that will be then executed by an external runner. The functions in `std.Build` implement a DSL for defining build steps and express dependencies between them, allowing the build runner to parallelize the build automatically (and the cache system to know when a step doesn't need to be re-run).
 pub fn build(b: *std.Build) void {
-    // Standard target options allow the person running `zig build` to choose
-    // what target to build for. Here we do not override the defaults, which
-    // means any target is allowed, and the default is native. Other options
-    // for restricting supported target set are available.
+    /// Standard target options allow the person running `zig build` to choose what target to build for. Here we do not override the defaults, which means any target is allowed, and the default is native. Other options for restricting supported target set are available.
     const target = b.standardTargetOptions(.{});
-    // Standard optimization options allow the person running `zig build` to select
-    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall. Here we do not
-    // set a preferred release mode, allowing the user to decide how to optimize.
+    // Standard optimization options allow the person running `zig build` to select between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall. Here we do not set a preferred release mode, allowing the user to decide how to optimize.
     const optimize = b.standardOptimizeOption(.{});
     // It's also possible to define more custom flags to toggle optional features
     // of this build script using `b.option()`. All defined flags (including
@@ -135,22 +125,12 @@ pub fn build(b: *std.Build) void {
     // A run step that will run the second test executable.
     const run_exe_tests = b.addRunArtifact(exe_tests);
 
-    // A top level step for running all tests. dependOn can be called multiple
-    // times and since the two run steps do not depend on one another, this will
-    // make the two of them run in parallel.
+    // A top level step for running all tests. dependOn can be called multiple times and since the two run steps do not depend on one another, this will make the two of them run in parallel.
     const test_step = b.step("test", "Run tests");
     test_step.dependOn(&run_mod_tests.step);
     test_step.dependOn(&run_exe_tests.step);
 
     // Just like flags, top level steps are also listed in the `--help` menu.
-    //
-    // The Zig build system is entirely implemented in userland, which means
-    // that it cannot hook into private compiler APIs. All compilation work
-    // orchestrated by the build system will result in other Zig compiler
-    // subcommands being invoked with the right flags defined. You can observe
-    // these invocations when one fails (or you pass a flag to increase
-    // verbosity) to validate assumptions and diagnose problems.
-    //
-    // Lastly, the Zig build system is relatively simple and self-contained,
-    // and reading its source code will allow you to master it.
+    // The Zig build system is entirely implemented in userland, which means that it cannot hook into private compiler APIs. All compilation work orchestrated by the build system will result in other Zig compiler subcommands being invoked with the right flags defined. You can observe these invocations when one fails (or you pass a flag to increase verbosity) to validate assumptions and diagnose problems.
+    // Lastly, the Zig build system is relatively simple and self-contained, and reading its source code will allow you to master it.
 }
